aec5f89add0aeb86a25c854e5c4ebf5ccc3a6c34
Mohammed Ashraf <tanviralam758@gmail.com>
2024-10-31 21:34:18 +0600

    some changes

---
+++ 31_default arguments.py
@@ -0,0 +1,4 @@
+# default arguments = A default value for certain parameters 
+#              default is used when that argument is omitted
+# make your functions more felxible, reduces number of arguments
+#  1. positioinal 2. DEFAULT 3. keyword 4. arbitary
